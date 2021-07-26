1c30b75ba631bbb21ed1330479cbbb049eed7546
Alexander Glück <github@alexanderglueck.at>
2021-07-26 19:33:26 +0200

    Add new flash message translations

--- de/flash_message.php
+++ de/flash_message.php
@@ -4,7 +4,81 @@ return [
     'settings' => [
         'profile' => [
             'updated' => 'Profil aktualisiert!',
-            'not_updated' => 'Profil konnte nicht aktualisiert werden!'
+            'not_updated' => 'Profil konnte nicht aktualisiert werden!',
+            'not_verified' => 'Ihre E-Mail-Adresse wurde noch nicht bestätigt. Sie können eine neue Bestätigungsemail in Ihren Profi-Einstellungen anfordern.'
+        ],
+        'two_factor_authentication' => [
+            'devices' => [
+                'deleted' => 'Geräte gelöscht!'
+            ],
+            'device' => [
+                'deleted' => 'Gerät gelöscht!'
+            ]
+        ],
+        'session' => [
+            'session' => [
+                'deleted' => 'Sitzung beendet!'
+            ],
+            'sessions' => [
+                'deleted' => 'Sitzungen beendet!'
+            ]
+        ],
+        'api_token' => [
+            'changed' => 'API-Schlüssel geändert!'
+        ],
+        'user_setting' => [
+            'deleted' => 'Konto gelöscht!',
+            'not_deleted' => 'Konto konnte nicht gelöscht werden!'
+        ],
+        'password' => [
+            'updated' => 'Passwort aktualisiert!',
+            'not_updated' => 'Passwort konnte nicht aktualisiert werden!'
+        ],
+        'subscription' => [
+            'cancelled' => 'Abonnement gekündigt!',
+            'resumed' => 'Abonnement fortgesetzt!',
+            'subscribed' => 'Vielen Dank für Ihr Abonnement!'
+        ],
+        'card' => [
+            'updated' => 'Karteninformationen aktualisiert!'
+        ],
+        'plan' => [
+            'changed' => 'Abonnement geändert!'
         ]
+    ],
+    'form' => [
+        'duplicated' => 'Formular dupliziert!',
+        'created' => 'Formular erstellt!',
+        'updated' => 'Formular aktualisiert!',
+        'deleted' => 'Formular gelöscht!',
+        'not_created' => 'Formular konnte nicht erstellt werden. Es sind Fehler bei der Validierung aufgetreten.',
+        'not_updated' => 'Formular konnte nicht aktualisiert werden. Es sind Fehler bei der Validierung aufgetreten.',
+
+        'api_token' => [
+            'created' => 'API-Schlüssel erstellt!',
+            'updated' => 'API-Schlüssel aktualisiert!',
+            'deleted' => 'API-Schlüssel gelöscht!',
+            'validation_failed' => 'API-Schlüssel konnte nicht erstellt/aktualisiert werden. Es sind Fehler bei der Validierung aufgetreten.'
+        ],
+
+        'page' => [
+            'updated' => 'Formular-Webseite aktualisiert!',
+            'enabled' => 'Formular-Webseite aktiviert!',
+            'validation_failed' => 'Formular-Webseite konnte nicht erstellt/aktualisiert werden. Es sind Fehler bei der Validierung aufgetreten.'
+        ],
+
+        'response' => [
+            'deleted' => 'Antwort gelöscht!',
+            'updated' => 'Antwort aktualisiert!'
+        ]
+    ],
+    'integration' => [
+        'enabled' => 'Integration aktiviert!',
+        'disabled' => 'Integration deaktiviert!'
+    ],
+    'auth' => [
+        'registered' => 'Vielen Dank für Ihre Anmeldung bei :name. Überprüfen Sie Ihre E-Mail-Adresse um Ihr Konto zu aktivieren.',
+        'verified' => 'Vielen Dank für das Bestätigen Ihrer E-Mail-Adresse!',
+        'expired' => 'Bestätigungslink ist ungültig oder abgelaufen.'
     ]
 ];
--- en/flash_message.php
+++ en/flash_message.php
@@ -4,7 +4,81 @@ return [
     'settings' => [
         'profile' => [
             'updated' => 'Profile updated!',
-            'not_updated' => 'Profile could not be updated!'
+            'not_updated' => 'Profile could not be updated!',
+            'not_verified' => 'Your email address has not been verified yet. You can resend the verification email on the profile settings page.'
+        ],
+        'two_factor_authentication' => [
+            'devices' => [
+                'deleted' => 'Devices deleted!'
+            ],
+            'device' => [
+                'deleted' => 'Device deleted!'
+            ]
+        ],
+        'session' => [
+            'session' => [
+                'deleted' => 'Session destroyed!'
+            ],
+            'sessions' => [
+                'deleted' => 'Sessions destroyed!'
+            ]
+        ],
+        'api_token' => [
+            'changed' => 'API Token changed!'
+        ],
+        'user_setting' => [
+            'deleted' => 'Profile deleted!',
+            'not_deleted' => 'Profile could not be deleted!'
+        ],
+        'password' => [
+            'updated' => 'Password updated!',
+            'not_updated' => 'Password could not be updated!'
+        ],
+        'subscription' => [
+            'cancelled' => 'Your subscription has been cancelled.',
+            'resumed' => 'Your subscription has been resumed.',
+            'subscribed' => 'Thanks for becoming a subscriber!'
+        ],
+        'card' => [
+            'updated' => 'Your card has been updated!'
+        ],
+        'plan' => [
+            'changed' => 'Your plan has been changed!'
         ]
+    ],
+    'form' => [
+        'duplicated' => 'Form duplicated!',
+        'created' => 'Form created!',
+        'updated' => 'Form updated!',
+        'deleted' => 'Form deleted!',
+        'not_created' => 'Form not created. There were validation errors.',
+        'not_updated' => 'Form not updated. There were validation errors.',
+
+        'api_token' => [
+            'created' => 'API Token created!',
+            'updated' => 'API Token updated!',
+            'deleted' => 'API Token deleted!',
+            'validation_failed' => 'API token not created/updated. There were validation errors.'
+        ],
+
+        'page' => [
+            'updated' => 'Page updated!',
+            'enabled' => 'Page enabled!',
+            'validation_failed' => 'Page not created/updated. There were validation errors.'
+        ],
+
+        'response' => [
+            'deleted' => 'Response deleted!',
+            'updated' => 'Response updated!'
+        ]
+    ],
+    'integration' => [
+        'enabled' => 'Integration enabled!',
+        'disabled' => 'Integration disabled!'
+    ],
+    'auth' => [
+        'registered' => 'Thanks for signing up to :name. Check your emails to activate your account.',
+        'verified' => 'Thanks for verifying your email address!',
+        'expired' => 'Token expired.'
     ]
 ];
