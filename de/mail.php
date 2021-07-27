2a3354e92931d651512b8033f4525931d9aaa5af
Alexander Glück <github@alexanderglueck.at>
2021-07-27 21:45:56 +0200

    Improve translations

--- de/mail.php
+++ de/mail.php
@@ -7,7 +7,7 @@ return [
         'message' => 'Vielen Dank dass Sie sich für :name entschieden haben.
 
 Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
-        'closing' => 'Mit freundlichen Grüßen,<br>:name'
+        'closing' => 'Mit freundlichen Grüßen, :name'
     ],
 
     'account' => [
@@ -16,7 +16,7 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
                 'subject' => 'E-Mail-Adresse aktualisiert',
                 'greeting' => 'E-Mail-Adresse aktualisiert!',
                 'message' => "Wir melden uns bei Ihnen, um Ihnen mitzuteilen, dass Ihre E-Mail-Adresse sich geändert hat.",
-                'closing' => 'Mit freundlichen Grüßen,<br>:name'
+                'closing' => 'Mit freundlichen Grüßen, :name'
             ]
         ],
 
@@ -25,7 +25,7 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
                 'subject' => 'Passwort aktualisiert',
                 'greeting' => 'Passwort aktualisiert!',
                 'message' => "Wir melden uns bei Ihnen, um Ihnen mitzuteilen, dass Ihr Passwort sich geändert hat.",
-                'closing' => 'Mit freundlichen Grüßen,<br>:name'
+                'closing' => 'Mit freundlichen Grüßen, :name'
             ]
         ],
 
@@ -34,13 +34,13 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
                 'subject' => 'Zwei-Faktor-Authentifizierung aktiviert',
                 'greeting' => 'Zwei-Faktor-Authentifizierung aktiviert!',
                 'message' => "Wir melden uns bei Ihnen, um Ihnen mitzuteilen, dass die Zwei-Faktor-Authentifizierung aktiviert wurde.",
-                'closing' => 'Mit freundlichen Grüßen,<br>:name'
+                'closing' => 'Mit freundlichen Grüßen, :name'
             ],
             'disabled' => [
                 'subject' => 'Zwei-Faktor-Authentifizierung deaktiviert',
                 'greeting' => 'Zwei-Faktor-Authentifizierung deaktiviert!',
                 'message' => "Wir melden uns bei Ihnen, um Ihnen mitzuteilen, dass die Zwei-Faktor-Authentifizierung deaktiviert wurde.",
-                'closing' => 'Mit freundlichen Grüßen,<br>:name'
+                'closing' => 'Mit freundlichen Grüßen, :name'
             ]
         ]
     ],
@@ -50,8 +50,8 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
             'subject' => 'Bestätigen Sie Ihre E-Mail-Adresse',
             'greeting' => 'Bestätigen Sie Ihre E-Mail-Adresse!',
             'message' => "Bitte bestätigen Sie Ihre E-Mail-Adresse, um Zugriff weitere :name Funktionen zu erhalten.",
-            'closing' => 'Mit freundlichen Grüßen,<br>:name',
-            'cta' => 'Bestätigen'
+            'closing' => 'Mit freundlichen Grüßen, :name',
+            'cta' => 'E-Mail-Adresse bestätigen'
         ]
     ],
 
@@ -60,14 +60,14 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
             'subject' => 'Neue Antwort erhalten',
             'greeting' => 'Neue Antwort erhalten!',
             'message' => 'Sie haben gerade eine neue Antwort in Ihrem ":form" Formular erhalten.',
-            'closing' => 'Mit freundlichen Grüßen,<br>:name',
+            'closing' => 'Mit freundlichen Grüßen, :name',
             'cta' => 'Alle Antworten anzeigen'
         ],
         'summary' => [
             'subject' => 'Neue Antworten erhalten',
             'greeting' => 'Neue Antworten erhalten!',
             'message' => "Das haben Sie verpasst:",
-            'closing' => 'Mit freundlichen Grüßen,<br>:name',
+            'closing' => 'Mit freundlichen Grüßen, :name',
             'cta' => ':name anzeigen',
             'notifications' => 'Benachrichtigungen',
             'view_response' => 'Antwort anzeigen'
@@ -81,7 +81,12 @@ Kontaktieren Sie uns, wenn Sie Hilfe benötigen.',
             'message' => 'Vielen Dank für Ihr Abonnement.
 
 Wir hoffen, Sie sind so glücklich wie wir. ',
-            'closing' => 'Mit freundlichen Grüßen,<br>:name'
+            'closing' => 'Mit freundlichen Grüßen, :name'
         ]
+    ],
+
+    'layout' => [
+        'copyright' => 'Alle Rechte vorbehalten.',
+        'subcopy' => "Wenn Sie Probleme beim Klicken des \":actionText\" Buttons haben, kopieren Sie die nachfolgende URL und fügen Sie die URL in die Adresszeile Ihres Browsers ein:"
     ]
 ];
--- en/mail.php
+++ en/mail.php
@@ -7,7 +7,7 @@ return [
         'message' => 'Thanks for signing up to :name.
 
 Please let us know if there is anything we can help you with.',
-        'closing' => 'Thanks,<br>:name'
+        'closing' => 'Thanks, :name'
     ],
 
     'account' => [
@@ -16,7 +16,7 @@ Please let us know if there is anything we can help you with.',
                 'subject' => 'E-Mail Address updated',
                 'greeting' => 'E-Mail Address updated!',
                 'message' => "We're just letting you know that your email was updated.",
-                'closing' => 'Thanks,<br>:name'
+                'closing' => 'Thanks, :name'
             ]
         ],
 
@@ -25,7 +25,7 @@ Please let us know if there is anything we can help you with.',
                 'subject' => 'Password updated',
                 'greeting' => 'Password updated!',
                 'message' => "We're just letting you know that your password was updated.",
-                'closing' => 'Thanks,<br>:name'
+                'closing' => 'Thanks, :name'
             ]
         ],
 
@@ -34,24 +34,24 @@ Please let us know if there is anything we can help you with.',
                 'subject' => 'Two-factor authentication enabled',
                 'greeting' => 'Two-factor authentication enabled!',
                 'message' => "We're just letting you know that two factor authentication was added to your account.",
-                'closing' => 'Thanks,<br>:name'
+                'closing' => 'Thanks, :name'
             ],
             'disabled' => [
                 'subject' => 'Two-factor authentication disabled',
                 'greeting' => 'Two-factor authentication disabled!',
                 'message' => "We're just letting you know that two factor authentication was removed from your account.",
-                'closing' => 'Thanks,<br>:name'
+                'closing' => 'Thanks, :name'
             ]
         ]
     ],
 
     'auth' => [
         'activate' => [
-            'subject' => 'Please activate your account',
-            'greeting' => 'Please activate your account!',
-            'message' => "Please activate your e-mail address to get access to all :name features.",
-            'closing' => 'Thanks,<br>:name',
-            'cta' => 'Activate'
+            'subject' => 'Verify E-Mail Address',
+            'greeting' => 'Verify E-Mail Address!',
+            'message' => "Please verify your e-mail address to get access to all :name features.",
+            'closing' => "Thanks, :name",
+            'cta' => 'Verify E-Mail Address'
         ]
     ],
 
@@ -60,14 +60,14 @@ Please let us know if there is anything we can help you with.',
             'subject' => 'New response received',
             'greeting' => 'New response received!',
             'message' => 'You just received a new response on your ":form" form.',
-            'closing' => 'Thanks,<br>:name',
+            'closing' => 'Thanks, :name',
             'cta' => 'View all responses'
         ],
         'summary' => [
             'subject' => 'New response received',
             'greeting' => 'New response received!',
             'message' => "Here's what you missed:",
-            'closing' => 'Thanks,<br>:name',
+            'closing' => 'Thanks, :name',
             'cta' => 'Visit :name',
             'notifications' => 'Notifications',
             'view_response' => 'View response'
@@ -81,7 +81,12 @@ Please let us know if there is anything we can help you with.',
             'message' => 'Thank you for becoming a subscriber.
 
 We hope you are as happy as we are. ',
-            'closing' => 'Thanks,<br>:name'
+            'closing' => 'Thanks, :name'
         ]
+    ],
+
+    'layout' => [
+        'copyright' => 'All Rights reserved.',
+        'subcopy' => "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below into your web browser:"
     ]
 ];
